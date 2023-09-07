cc5ad61dbd087f46902bca86fd2e3df9529c7950
AlyxerHL <alyxer@alyxer.me>
2023-09-08 08:25:39 +0900

    Refactor codes

--- content.js
+++ content.js
@@ -1,5 +1,4 @@
 const BUTTONS_ROOT_QUERY = "div.PlayerCaption_player_caption_option__rCZXX";
-
 const LANGUAGE_STATE = {
     both: "한/A",
     english: "ABC",
@@ -7,99 +6,48 @@ const LANGUAGE_STATE = {
     hide: "숨김",
 };
 
-/**
- * @param {HTMLButtonElement} repeatButton
- * @param {boolean} state
- */
-const setRepeatState = (repeatButton, state) => {
-    if (repeatButton.ariaPressed !== state.toString()) {
-        repeatButton.click();
-    }
-};
+const findAndCreateButtons = () => {
+    const createStageButton = (stage, repeatState, languageState) => {
+        const button = document.createElement("button");
+        button.innerText = stage;
 
-/**
- * @param {HTMLButtonElement} languageButton
- * @param {string} state
- */
-const setLanguageState = async (languageButton, state) => {
-    while (languageButton.innerText !== state) {
-        languageButton.click();
-        await new Promise((resolve) => setTimeout(resolve, 0));
-    }
-};
+        button.addEventListener("click", async () => {
+            if (repeat.ariaPressed !== repeatState.toString()) {
+                repeat.click();
+            }
 
-/**
- * @typedef {{ root: HTMLDivElement, repeat: HTMLButtonElement, language: HTMLButtonElement }} Buttons
- * @returns {Buttons | null}
- */
-const findButtons = () => {
+            while (language.innerText !== languageState) {
+                language.click();
+                await new Promise((resolve) => setTimeout(resolve, 0));
+            }
+        });
+
+        return button;
+    };
+
     const root = document.querySelector(BUTTONS_ROOT_QUERY);
+    if (!root) {
+        return null;
+    }
+
     const repeat = document.querySelector(`${BUTTONS_ROOT_QUERY} :nth-child(2)`);
     const language = document.querySelector(`${BUTTONS_ROOT_QUERY} :nth-child(3)`);
-    return !!root && !!repeat && !!language ? { root, repeat, language } : null;
-};
+    const first = createStageButton(1, false, LANGUAGE_STATE.korean);
+    const second = createStageButton(2, true, LANGUAGE_STATE.both);
+    const third = createStageButton(3, false, LANGUAGE_STATE.hide);
 
-/**
- * @param {Buttons} buttons
- * @returns {HTMLButtonElement}
- */
-const createFirstButton = (buttons) => {
-    const firstButton = document.createElement("button");
-    firstButton.innerText = "1";
-
-    firstButton.addEventListener("click", () => {
-        setRepeatState(buttons.repeat, false);
-        setLanguageState(buttons.language, LANGUAGE_STATE.korean);
-    });
-
-    return firstButton;
-};
-
-/**
- * @param {Buttons} buttons
- * @returns {HTMLButtonElement}
- */
-const createSecondButton = (buttons) => {
-    const secondButton = document.createElement("button");
-    secondButton.innerText = "2";
-
-    secondButton.addEventListener("click", () => {
-        setRepeatState(buttons.repeat, true);
-        setLanguageState(buttons.language, LANGUAGE_STATE.both);
-    });
-
-    return secondButton;
-};
-
-/**
- * @param {Buttons} buttons
- * @returns {HTMLButtonElement}
- */
-const createThirdButton = (buttons) => {
-    const thirdButton = document.createElement("button");
-    thirdButton.innerText = "3";
-
-    thirdButton.addEventListener("click", () => {
-        setRepeatState(buttons.repeat, false);
-        setLanguageState(buttons.language, LANGUAGE_STATE.hide);
-    });
-
-    return thirdButton;
+    return { root, first, second, third };
 };
 
 chrome.runtime.onMessage.addListener((message) => {
     if (message === "comprehensible-cake") {
-        const buttons = findButtons();
+        const buttons = findAndCreateButtons();
         if (!buttons) {
             return;
         }
 
-        const firstButton = createFirstButton(buttons);
-        const secondButton = createSecondButton(buttons);
-        const thirdButton = createThirdButton(buttons);
-
-        buttons.root.appendChild(firstButton);
-        buttons.root.appendChild(secondButton);
-        buttons.root.appendChild(thirdButton);
+        buttons.root.appendChild(buttons.first);
+        buttons.root.appendChild(buttons.second);
+        buttons.root.appendChild(buttons.third);
     }
 });
